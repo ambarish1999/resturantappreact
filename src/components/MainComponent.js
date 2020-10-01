64c69b02ad84e2e6e838da16db7980d0775c49ba
amberbanerjee <akb14@iitbbs.ac.in>
2020-10-01 19:55:43 +0530

    react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import { DISHES } from '../shared/dishes';
-import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Menu from './MenuComponent';
+import { Switch,  Route, Redirect  } from 'react-router-dom';
 class Main extends Component {
 
   constructor(props){
       super();
 
       this.state={
-        dishes: DISHES,
-        selectedDishid : null
+        dishes: DISHES
       };
   }
-  onClickDish(dishid){
-    this.setState({selectedDishid : dishid});
-  }
   render(){
+    const HomePage= ()=> {
+        return (
+            <Home />
+        );
+    }
     return (
       <div className="Main">
         <Header/>
-        <Menu dishes={this.state.dishes}  onClick={(dishId)=>{
-          this.onClickDish(dishId)
-        }}/>
-        <DishDetail selectedDish={ this.state.dishes.filter((dish)=>
-          dish.id===this.state.selectedDishid )[0]} />
+        <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,7 +8,8 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
         const menu = props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card key={dish.id} onClick={()=> props.onClick(dish.id)}>
+                {/* <Card key={dish.id} onClick={()=> props.onClick(dish.id)}> */}
+                <Card key={dish.id}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
